cf70874c7704ac045bc0b3f133373ffc4ef85ed2
Sumithra-Sudhakar <sumisooriya@gmail.com>
2021-10-27 17:42:01 +0530

    few more files added

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:parking_slot_booking/login.dart';
 import 'package:parking_slot_booking/parked_in.dart';
 import 'package:parking_slot_booking/parked_out.dart';
+import 'package:parking_slot_booking/search_page.dart';
+import 'package:parking_slot_booking/sign_up.dart';
 
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: SafeArea(
-      child: Login(),
+      child: SignUp(),
     ),
   ));
 }
---
+++ lib/search_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Search extends StatefulWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  Icon customIcon = Icon(Icons.search);
+  Widget customSearchBar = Text("where do you want to go today?", style: GoogleFonts.alfaSlabOne(),);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: customSearchBar,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(onPressed:  (){
+            setState(() {
+              if (customIcon.icon == Icons.search){
+                customIcon = Icon(Icons.cancel);
+                customSearchBar = const ListTile(
+                  leading: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                  title: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'type in the place...',
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontStyle: FontStyle.italic,
+                      ),
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                );
+
+
+              }
+              else{
+                customIcon = Icon(Icons.search);
+                customSearchBar = Text("where are you going today?", style: GoogleFonts.alfaSlabOne(),);
+              }
+            });
+
+          }, icon:customIcon),
+        ],
+        centerTitle: true,
+      ),
+    );
+  }
+}
